2705c16a93bdd914be78427e59e935b318848076
Iagor Moraes <iagor2014@outlook.com>
2024-03-25 16:39:02 -0300

    feat: added NR

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: [/(react-imask|imask)/, /node-fetch/],
+  serverDependenciesToBundle: [/(react-imask|imask)/, /node-fetch/, /newrelic/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
