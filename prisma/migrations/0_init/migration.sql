471b30ab763bdba60c1c006b3e1b844834ffbc11
Iagor Moraes <iagor2014@outlook.com>
2024-05-09 10:45:52 -0300

    chore: add prisma

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "State" (
+    "uuid" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "State_pkey" PRIMARY KEY ("uuid")
+);
+
