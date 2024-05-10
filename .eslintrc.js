bc798ec3f38373ad47f6f557bb8599c11bfd5be1
Iagor Moraes <iagor2014@outlook.com>
2024-05-10 04:40:30 -0300

    refactor: fix order of client generation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@unumid/eslint-config-react', 'prettier'],
-  ignorePatterns: ['build', 'app/generated/client'],
+  ignorePatterns: ['build'],
 };
