f4a71fb481cae2715f4deb4b136dbc8d16e3caf0
Iagor Moraes <iagor2014@outlook.com>
2024-05-13 11:47:31 -0300

    fix: add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Debug database url
+echo DATABASE_URL
+
+# Run the database migrations
+prisma migrate deploy
+
+# Run the app
+cross-env NODE_ENV=production node -r newrelic ./server.js
